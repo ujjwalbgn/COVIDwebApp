7ac1bd29a7c2478ba204fdffff78a32ee04db279
Ujjwal Bajagain <ubajagain@gmail.com>
2020-08-02 04:03:39 -0500

    user login and registration created

--- healthcare/forms.py
+++ healthcare/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.models import User
 from .models import Patient
 
 class PatientForm(ModelForm):
     class Meta:
         model = Patient
         fields = '__all__'
+        exclude= ['user']
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1','password2']
--- healthcare/views.py
+++ healthcare/views.py
@@ -1,10 +1,60 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
 
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group, User
+
+
+
 from .models import Patient
 from .forms import *
 # Create your views here.
 
+def registerPage(request):
+
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+
+            messages.success(request,'Account was created for ' + username)
+
+            return redirect('login')
+
+    context= {'form': form}
+    return render(request, 'healthcare/register.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username = username,password = password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username or Password is incorrect')
+
+    content= {}
+    return render(request, 'healthcare/login.html', content)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def home(request):
     return render(request, 'healthcare/home.html')
