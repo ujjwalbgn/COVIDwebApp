d3589f835f0a182cee48f8824271616abb298d19
Ujjwal Bajagain <ubajagain@gmail.com>
2020-08-02 04:03:39 -0500

    user login and registration created

--- healthcare/urls.py
+++ healthcare/urls.py
@@ -4,6 +4,11 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
+
     path('', views.home, name="home"),
     path('edit_pateint/<str:pk>/', views.editPateint, name="editPateint"),
 ]
