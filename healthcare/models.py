cf29cda59149af516106e0d3c8989cc915fd007a
Ujjwal Bajagain <ubajagain@gmail.com>
2020-08-02 21:15:44 -0500

    fields added in patients model

--- healthcare/models.py
+++ healthcare/models.py
@@ -11,8 +11,16 @@ class Patient(models.Model):
     email = models.CharField(max_length=200, null= True)
     date_created = models.DateField(auto_now_add = True)
 
-
-    #todo add patients medical history
+    birth_date = models.CharField(max_length=10, null=True)
+    age = models.DecimalField(decimal_places=0, max_digits=50, null=True)
+    gender = models.CharField(max_length=10, default='')
+    height = models.CharField(max_length=10, default='0.00 ft')
+    weight = models.CharField(max_length=10, default='0.00 lbs')
+    allergies = models.CharField(max_length=200, null=True)
+    emergency_contact_name = models.CharField(max_length=200, null=True)
+    emergency_contact_phone = models.CharField( max_length=200, null=True)
+    emergency_contact_address = models.CharField(max_length=200, null=True)
+    insurance_provider = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         if (self.firstName and self.lastName):
