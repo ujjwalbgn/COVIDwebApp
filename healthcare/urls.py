8254f82792be4d4764c02915c42ddd24bb3d4e28
Ujjwal Bajagain <ubajagain@gmail.com>
2020-08-04 16:25:58 -0500

    med, treatment and all pateint created, registration error fixed

--- healthcare/urls.py
+++ healthcare/urls.py
@@ -10,12 +10,13 @@ urlpatterns = [
 
 
     path('', views.home, name="home"),
-    path('edit_patient/<int:pk>/', views.editPateint, name="editPatient"),
-
+    path('edit_patient/<str:pk>/', views.editPatient, name="editPatient"),
+    path('listpatient/', views.listPatient, name="listPatient"),
 
     path('testlocation/', views.testLocation, name="testLocation"),
     path('covidscreening/', views.covidScreening, name="covidScreening"),
 
     path('medication/', views.editMedication, name="medication"),
+    path('treatment/', views.editTreatement, name="treatment"),
 
 ]
