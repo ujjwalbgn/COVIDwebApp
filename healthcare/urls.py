768075df6e0dfd5e56f569ad3d2dd037b05500cc
Ujjwal Bajagain <ubajagain@gmail.com>
2020-08-04 17:13:08 -0500

    added forget password features

--- healthcare/urls.py
+++ healthcare/urls.py
@@ -9,6 +9,19 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
 
 
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="healthcare/password_reset.html"),
+         name="reset_password"),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="healthcare/password_reset_sent.html"),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="healthcare/password_reset_form.html"),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="healthcare/password_reset_done.html"),
+         name="password_reset_complete"),
+
     path('', views.home, name="home"),
     path('edit_patient/<str:pk>/', views.editPatient, name="editPatient"),
     path('listpatient/', views.listPatient, name="listPatient"),
