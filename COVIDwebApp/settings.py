e81cff478365ea8268db51d6124127e386e4d815
unknown <alisha.kunwar@mavs.uta.edu>
2020-08-04 14:03:29 -0500

    forgot password added

--- COVIDwebApp/settings.py
+++ COVIDwebApp/settings.py
@@ -141,3 +141,11 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'covidtest321@gmail.com'
+EMAIL_HOST_PASSWORD = 'Covid#123'
--- healthcare/urls.py
+++ healthcare/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
@@ -12,5 +12,21 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('edit_patient/<int:pk>/', views.editPateint, name="editPatient"),
     path('testlocation/', views.testLocation, name="testLocation"),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="healthcare/password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="healthcare/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="healthcare/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="healthcare/password_reset_done.html"),
+         name="password_reset_complete"),
 
 ]
